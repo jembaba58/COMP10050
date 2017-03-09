84c49ed049fcfe68ec141767e8a9f63ba7f7c291
jembaba58 <viking.jem@gmail.com>
2017-03-09 16:22:34 +0000

    Update submitting.c

--- submitting.c
+++ submitting.c
@@ -29,7 +29,102 @@ struct slot
 	char type[20]; //The type of a slot could be: Level Ground, Hill or City.
 };
 
+void pointsHuman(struct players[])
+{
+	int smartness, strength, skill, luck, dexterity;
+	int sum;
+	int numPlayers;
+	
+	for(i=0; i<numPlayers; i++)
+	{
+		if(players[i].player_type ="Human")
+		{
+				do while(sum < 300)
+					{
+	
+							players[i].smartness = 1 +(rand()%100);
+						
+							players[i].strength = 1 +(rand()%100);
+						
+							players[i].skill = 1 +(rand()%100);
+						
+							players[i].dexterity = 1 +(rand()%100);
+						
+							players[i].dexterity = 1 +(rand()%100);
+							
+							sum = players[i].smartness + players[i].strength + players[i].skill + players[i].luck + players[i].dexterity;
+					}
+		}
+	}
+	
+}
 
+void pointsOgre(struct players[])
+{
+	int smartness, strength, skill = 0, luck, dexterity;
+	
+	for(i=0; i<numPlayers; i++)
+	{
+		if(players[i].player_type = "Ogre")
+		{
+				players[i].strength = 1 +(rand()%80);
+				
+				players[i].dexterity = 1 +(rand()%80);
+			
+					do while(sum <= 50)
+					{
+						players[i].luck = 1 +(rand()%100);
+						
+						players[i].smartness = 1 +(rand()%20);
+						
+						sum = players[i].luck + players[i].smartness;
+					}
+		}
+	}
+	
+	
+}
+
+void pointsElf(struct players[])
+{
+	int smartness, strength, skill = 0, luck, dexterity;
+	
+	for(i=0; i<numPlayers; i++)
+	{
+		if(players[i].player_type = "Elf")
+		{
+	
+					players[i].luck = 60 +(rand()%40);
+					
+					players[i].smartness  = 70 +(rand()%30);
+						
+					players[i].strength = 1 +(rand()%50);
+						
+					players[i].skill = 50 +(rand()%30);
+		}
+	}
+	
+}
+
+void pointsWizard(struct players[])
+{
+	int smartness, strength, skill, luck, dexterity;
+	
+	for(i=0; i<numPlayers; i++)
+	{
+		if(players[i].player_type = "Wizard")
+		{
+			players[i].luck = 50 +(rand()%50);
+			
+			players[i].smartness = 90 +(rand()%10);
+				
+			players[i].strength = 1 +(rand()%20);
+			
+			players[i].skill = 80 +(rand()%20);
+		}
+	}
+		
+}
 
 
 
