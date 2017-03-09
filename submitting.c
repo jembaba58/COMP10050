b3a222e359b745088b90bf09e7bfcb4238d7c4f9
jembaba58 <viking.jem@gmail.com>
2017-03-09 18:02:05 +0000

    Update submitting.c

--- submitting.c
+++ submitting.c
@@ -47,8 +47,10 @@ void pointsHuman(struct players[])
 			sum = players[i].smartness + players[i].strength + players[i].skill + players[i].luck + players[i].dexterity;
 			
 			int check = 1;
-			while(check == 1){
-				if(human.dexterity + human.luck + human.magicSkills + human.smartness + human.strength >=300){
+			while(check == 1)
+			{
+				if(human.dexterity + human.luck + human.magicSkills + human.smartness + human.strength >=300)
+				{
 					players[i].smartness = rand()%100 + 1;
 					players[i].strength = rand()%100 + 1;
 					players[i].skill = rand()%100 + 1;
@@ -78,9 +80,11 @@ void pointsOgre(struct players[])
 			players[i].smartness = (rand()%20);
 			sum = players[i].luck + players[i].smartness;
 			
-			check = 1;
-			while(check == 1){
-				if(players[i].luck + players[i].smartness > 50){
+			int check = 1;
+			while(check == 1)
+			{
+				if(players[i].luck + players[i].smartness > 50)
+				{
 					players[i].luck = rand()%50;
 					players[i].smartness = rand()%20;
 				}
@@ -102,14 +106,10 @@ void pointsElf(struct players[])
 	{
 		if(players[i].player_type = "Elf")
 		{
-	
-					players[i].luck = 60 +(rand()%40);
-					
-					players[i].smartness  = 70 +(rand()%30);
-						
-					players[i].strength = 1 +(rand()%50);
-						
-					players[i].skill = 50 +(rand()%30);
+			players[i].luck = 60 +(rand()%40);
+			players[i].smartness  = 70 +(rand()%30);
+			players[i].strength = 1 +(rand()%50);
+			players[i].skill = 50 +(rand()%30);
 		}
 	}
 	
@@ -124,11 +124,8 @@ void pointsWizard(struct players[])
 		if(players[i].player_type = "Wizard")
 		{
 			players[i].luck = 50 +(rand()%50);
-			
 			players[i].smartness = 90 +(rand()%10);
-				
 			players[i].strength = 1 +(rand()%20);
-			
 			players[i].skill = 80 +(rand()%20);
 		}
 	}
