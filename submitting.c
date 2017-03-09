d2a3ff4526937d3ed929921a4557ae028bef8811
jembaba58 <viking.jem@gmail.com>
2017-03-09 16:10:25 +0000

    Update submitting.c

--- submitting.c
+++ submitting.c
@@ -2,3 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+
+void pointsHuman(struct players[]);
+void pointsOgre(struct players[]);
+void pointsElf(struct players[]);
+void pointsWizard(struct players[]);
+void slotType(struct slot[]);
+void allocatePlayers(struct players[]);
+void abs(int a);
+
+
+
+
+
+
+
+
