93805115a49e91157f49a135251d42096401bc0d
jembaba58 <viking.jem@gmail.com>
2017-03-31 12:17:39 +0100

    Update main.c

--- Software Engineering Project 3/main.c
+++ Software Engineering Project 3/main.c
@@ -24,16 +24,24 @@ int main(void)
 
 	createBoard(boardSize, &upLeft, &upRight, &downLeft, &downRight);
 
+	getDesiredElement(boardSize, &row,&column);
 
-
-
-
-
-
-
-
-
-
+	if(row >= boardSize/2){
+		if(column >= boardSize/2){
+			reachDesiredElement(row,column,downRight);
+		}
+		else{
+			reachDesiredElement(row,column,downLeft);
+		}
+	}
+	else{
+		if(column >= boardSize/2){
+			reachDesiredElement(row,column, upRight);
+		}
+		else{
+			reachDesiredElement(row,column,upLeft);
+		}
+	}
 
 	return 0;
 }
